68ea996818d9dd0f23f99b1950f45a2755445844
toppmote <toppmote@gmail.com>
2022-01-26 21:53:29 +0700

    Добавил логирование для всех методов + изменил возвращаемые сообщения об обработке каждого метода

--- src/main/java/ru/cheezeapp/controller/CrudController.java
+++ src/main/java/ru/cheezeapp/controller/CrudController.java
@@ -33,10 +33,13 @@ public class CrudController {
      */
     @PostMapping("/strain/delete/{id}")
     public String softDeletionOfStrainById(@PathVariable Long id) {
+        log.info("[POST /strain/delete/" + id + "]\tEntered softDeletionOfStrainById() method");
         try {
             strainCrudService.softDeletionOfStrainById(id);
-            return "good :)";
+            log.info("[POST /strain/delete/" + id + "]\tSoft deleted strain with id: " + id);
+            return "Штамм помещён в корзину";
         } catch (Exception e) {
+            log.info("[POST /strain/delete/" + id + "]\tThrows exception: " + e.getMessage());
             return e.getMessage();
         }
     }
@@ -49,10 +52,13 @@ public class CrudController {
      */
     @PostMapping("/strain/hard_delete/{id}")
     public String hardDeletionOfStrainById(@PathVariable Long id) {
+        log.info("[POST /strain/hard_delete/" + id + "]\tEntered hardDeletionOfStrainById() method");
         try {
             strainCrudService.hardDeletionOfStrainById(id);
-            return "good :)";
+            log.info("[POST /strain/hard_delete/" + id + "]\tHard deleted strain with id: " + id);
+            return "Штамм успешно удалён";
         } catch (Exception e) {
+            log.info("[POST /strain/hard_delete/" + id + "]\tThrows exception: " + e.getMessage());
             return e.getMessage();
         }
     }
@@ -66,17 +72,21 @@ public class CrudController {
      */
     @PostMapping("/strain/send")
     public String sendStrain(@RequestBody String strainJson) {
+        log.info("[POST /strain/send/]\tEntered sendStrain() method");
         try {
             StrainEntity strain = jsonToObjectConverter.JsonToStrain(strainJson);
             if (strain.getId() == 0) {
                 strainCrudService.addStrain(strain);
-                log.info("Strain was created, id = " + strainSearchService.findByName(strain.getExemplar()));
+                log.info("[POST /strain/send/]\tStrain was created, id = " +
+                        strainSearchService.findByName(strain.getExemplar()));
+                return "Штамм был успешно добавлен";
             } else {
                 strainCrudService.updateStrain(strain);
-                log.info("Strain was updated, id = " + strain.getId());
+                log.info("[POST /strain/send/]\tStrain was updated, id = " + strain.getId());
+                return "Штамм был успешно обновлен";
             }
-            return "good :)";
         } catch (Exception e) {
+            log.info("[POST /strain/send/]\tThrows exception: " + e.getMessage());
             return e.getMessage();
         }
     }
@@ -88,10 +98,13 @@ public class CrudController {
      */
     @PostMapping("/strain/hard_delete_all")
     public String hardDeleteAllStrains() {
+        log.info("[POST /strain/hard_delete_all/]\tEntered hardDeleteAllStrains() method");
         try {
             strainCrudService.hardDeleteAllStrains();
-            return "good :)";
+            log.info("[POST /strain/hard_delete_all/]\tAll deleted strains were hardly deleted");
+            return "Корзина очищена";
         } catch (Exception e) {
+            log.info("[POST /strain/hard_delete_all/]\tThrows exception: " + e.getMessage());
             return e.getMessage();
         }
     }
@@ -103,11 +116,14 @@ public class CrudController {
      * @return сообщение об обработке
      */
     @PostMapping("/strain/restore/{id}")
-    public String restoreStrain(@PathVariable long id) {
+    public String restoreStrain(@PathVariable Long id) {
+        log.info("[POST /strain/restore/" + id + "]\tEntered restoreStrain() method");
         try {
             strainCrudService.restoreStrain(id);
-            return "good :)";
+            log.info("[POST /strain/restore/" + id + "]\tStrain was restored from bucket");
+            return "Штамм восстновлен из корзины";
         } catch (Exception e) {
+            log.info("[POST /strain/restore/" + id + "]\tThrows exception: " + e.getMessage());
             return e.getMessage();
         }
     }
